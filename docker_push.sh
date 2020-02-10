5eaf1d78c4efea5a7f2d55a4e5f60af7dc9baa48
patelvp <patel.visha@husky.neu.edu>
2020-02-09 23:15:45 -0500

    update to docker push file

--- docker_push.sh
+++ docker_push.sh
@@ -2,4 +2,4 @@
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 docker build -t climatetree:api-gateway .
 docker tag climatetree:api-gateway patelvp/climatetree:api-gateway
-docker push patelvp/climatetree
+docker push patelvp/climatetree:api-gateway
